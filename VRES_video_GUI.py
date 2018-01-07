9a16ca4d7ce684af8516aecf749ace64b923b64c
KMoyle <kylesmoyle@gmail.com>
2018-01-07 17:00:17 +1000

    Add files via upload

--- VRES_video_GUI.py
+++ VRES_video_GUI.py
@@ -9,7 +9,7 @@ import matplotlib
 import soundfile as sf 
 
 from numpy import arange
-from Tkinter import Tk, Label, Button, Frame, N, S, W, E 
+from Tkinter import Tk, Label, Button, Frame, N, S, W, E, Entry
 from PIL import Image, ImageTk
 from scipy.io import wavfile as wav
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
@@ -27,51 +27,65 @@ class VideoProcessing:
 		#load in video one and extract frames 
 		vid = cv2.VideoCapture(vid_url)
 		
-		success,frame = vid.read()
+		#success,frame = vid.read()
 
 		self.fps = int(vid.get(cv2.CAP_PROP_FPS))
 		self.frame_total = (int(vid.get(cv2.CAP_PROP_FRAME_COUNT)))- 3 
 		
-		count = 0
-		success = True
-		while success:
-			success,frame = vid.read()
+
+		#print(self.fps)
+		#count = 0
+		#success = True
+		#while success:
+		#	success,frame = vid.read()
 			
-			if success:
-				gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-				gray_resized = cv2.resize(gray, (640, 480))
+		#	if success:
+		#		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+		#		gray_resized = cv2.resize(gray, (640, 480))
 
-			print('Reading frame number %d' % count)
+		#	print('Reading frame number %d' % count)
 
-			cv2.imwrite(name + "_frame_%d.jpg" % count, gray_resized)     # save frame as JPEG file
-			if cv2.waitKey(1) & 0xFF == ord('q'):
-				break
-			count += 1
+		#	cv2.imwrite(name + "_frame_%d.jpg" % count, gray_resized)     # save frame as JPEG file
+		#	if cv2.waitKey(1) & 0xFF == ord('q'):
+		#		break
+		#	count += 1
 
 		#load in video and change to .wav for processing
-		command_1 = "ffmpeg -i /home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.mov -acodec copy -vcodec copy sync_iphone.avi"
-		command_2 = "ffmpeg -i /home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.avi -ab 160k -ac 2 -ar 44100 -vn sync_iphone.wav"
-		subprocess.call(command_1, shell=True)
-		subprocess.call(command_2, shell=True)
+		if vid_url == '/home/kylesm/Desktop/VRES/VRES_GUI/sync_sony.MTS':
+		#	command_1 = "ffmpeg -i " + vid_url + " -acodec copy -vcodec copy sync_sony.avi"
+		#	print(command_1)
+		#	command_2 = "ffmpeg -i /home/kylesm/Desktop/VRES/VRES_GUI/sync_sony.avi -ab 160k -ac 2 -ar 44100 -vn sync_sony.wav"
+		#	subprocess.call(command_1, shell=True)
+		#	subprocess.call(command_2, shell=True)
 
-		spf = wave.open("/home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.wav",'r')
+			spf = wave.open("/home/kylesm/Desktop/VRES/VRES_GUI/sync_sony.wav",'r')
 
+		elif vid_url == '/home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.mov':
+		#	command_1 = "ffmpeg -i " + vid_url + " -acodec copy -vcodec copy sync_iphone.avi"
+		#	print(command_1)
+		#	command_2 = "ffmpeg -i /home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.avi -ab 160k -ac 2 -ar 44100 -vn sync_iphone.wav"
+		#	subprocess.call(command_1, shell=True)
+		#	subprocess.call(command_2, shell=True)
+
+			spf = wave.open("/home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.wav",'r')
 		#Extract Raw Audio from Wav File
 		self.wav_signal = spf.readframes(-1)
 		self.wav_signal = np.fromstring(self.wav_signal, 'Int16')
-
+		
 		#creating a figure to house the audio signal plot
 		self.f = Figure(figsize=(8, 5), dpi=100)
 		self.a = self.f.add_subplot(111)
+		self.plot_length = len(self.wav_signal)
+		float(self.plot_length)
+		print(self.plot_length)
 
 		#calculating the step size of the frames
-		float(self.frame_total)
-		self.length = self.frame_total/1757184.0
-
+		self.length = float(self.frame_total)/float(self.plot_length)
+		print(self.frame_total) 
+		print(self.length)
 		#calculating time vector
-		self.t = arange(0, self.frame_total, self.length) 
+		self.t = arange(0, self.frame_total, self.length)
 
-	def loadAudio(self,)
 
 
 #localisation application class
@@ -104,7 +118,17 @@ class CameraLocGUI(object,VideoProcessing):
 		QRY.a.set_xlabel('Frame')
 		QRY.a.set_ylabel('Amplitude')
 
-		# setting frames
+		# setting frame search entry
+		self.REF_entry = Entry(parent)
+		self.QRY_entry = Entry(parent)
+
+		Label(parent, text="Go to REF frame",font=("Helvetica", 14), fg="red").grid(row=5, column=0, sticky=E)
+		Label(parent, text="Go to QRY frame",font=("Helvetica", 14), fg="red").grid(row=5, column=6, sticky=E)
+
+		Button(parent, text='   GO   ', command=lambda: self.go_to_frame("REF_search")).grid(row=5, column=2, sticky=W+E,)
+		Button(parent, text='   GO   ', command=lambda: self.go_to_frame("QRY_search")).grid(row=5, column=8, sticky=W+E,columnspan=2)
+
+		# setting image frames
 		self.REF_label = Label(parent, image=REF_photo)
 		self.REF_label.image = REF_photo
 		self.REF_label_index = 0
@@ -113,7 +137,7 @@ class CameraLocGUI(object,VideoProcessing):
 		self.QRY_label.image = QRY_photo
 		self.QRY_label_index = 0
 
-		# setting buttons
+		# setting scale buttons
 		self.REF_scale_up_button = Button(parent, text="  Next Frame ", command=lambda: self.update("REF_scale_up", REF, QRY) )
 		self.REF_scale_down_button = Button(parent, text="Previous Frame", command=lambda: self.update("REF_scale_down", REF, QRY) )
 		self.QRY_scale_up_button = Button(parent, text="  Next Frame ", command=lambda: self.update("QRY_scale_up", REF, QRY) )
@@ -123,16 +147,19 @@ class CameraLocGUI(object,VideoProcessing):
 		self.REF_label.grid(row=1, column=0,columnspan=6,rowspan=2)
 		self.QRY_label.grid(row=1, column=6, columnspan=6,rowspan=2)
 
-		self.REF_scale_down_button.grid(row=4, column=0, columnspan=3,rowspan=1, padx=15, pady=15)
-		self.REF_scale_up_button.grid(row=4, column=3, columnspan=3,rowspan=1, padx=15, pady=15)
-		self.QRY_scale_down_button.grid(row=4, column=6, columnspan=3,rowspan=1, padx=15, pady=15)
-		self.QRY_scale_up_button.grid(row=4, column=9, columnspan=3, rowspan=1, padx=15, pady=15)
+		self.REF_scale_down_button.grid(row=4, column=0,columnspan=2, pady=15)
+		self.REF_scale_up_button.grid(row=4, column=2, columnspan=2,  pady=15)
+		self.QRY_scale_down_button.grid(row=4, column=6, columnspan=2,  pady=15)
+		self.QRY_scale_up_button.grid(row=4, column=8, columnspan=2,  pady=15)
 
-		#setting frame titles
-		REF_title = Label(parent, text='REF Frame', font=("Helvetica", 16), fg="red")
+		self.REF_entry.grid(row=5, column=1, sticky=W)
+		self.QRY_entry.grid(row=5, column=7, sticky=W)
+
+		#setting frame titles and corresponding number
+		REF_title = Label(parent, text='REF Frame\n\nfps = %d  frames = %d' % (REF.fps, REF.frame_total), font=("Helvetica", 16), fg="red")
 		REF_title.grid(row=0, column=0,columnspan=6)
 
-		QRY_title = Label(parent, text='QRY Frame', font=("Helvetica", 16), fg="red")
+		QRY_title = Label(parent, text='QRY Frame\n\nfps = %d  frames = %d' % (QRY.fps, QRY.frame_total), font=("Helvetica", 16), fg="red")
 		QRY_title.grid(row=0, column=6,columnspan=6)
 
 		self.REF_number = Label(parent, font=("Helvetica", 12), fg="red")
@@ -143,15 +170,14 @@ class CameraLocGUI(object,VideoProcessing):
 		self.QRY_number.grid(row=3, column=6,columnspan=6)
 		self.QRY_number.configure(text='Frame Number %d' % self.QRY_label_index)
 
-
-		#Making frames for NAV bar
+		# frame object for NAV bar
 		toolbar_frame_1 = Frame(parent)
 		toolbar_frame_2 = Frame(parent)
 		
 		# a tk.DrawingArea
 		REF_canvas = FigureCanvasTkAgg(REF.f, master=parent)
 		REF_canvas.show()
-		REF_canvas.get_tk_widget().grid(row=5, column=0,columnspan=6, rowspan=2, padx=5, pady=5)
+		REF_canvas.get_tk_widget().grid(row=6, column=0,columnspan=6, rowspan=2, padx=5, pady=5)
 
 		toolbar = NavigationToolbar2TkAgg(REF_canvas, toolbar_frame_1)
 		toolbar.update()
@@ -161,7 +187,7 @@ class CameraLocGUI(object,VideoProcessing):
 
 		QRY_canvas = FigureCanvasTkAgg(QRY.f, master=parent)
 		QRY_canvas.show()
-		QRY_canvas.get_tk_widget().grid(row=5, column=6, columnspan=6, rowspan=1,padx=5, pady=5)
+		QRY_canvas.get_tk_widget().grid(row=6, column=6, columnspan=6, rowspan=1,padx=5, pady=5)
 
 		toolbar_1 = NavigationToolbar2TkAgg(QRY_canvas, toolbar_frame_2)
 		toolbar_1.update()
@@ -234,13 +260,33 @@ class CameraLocGUI(object,VideoProcessing):
 		# updating the frame numbers txt
 		self.REF_number.configure(text='Frame Number %d' % self.REF_label_index)
 		self.QRY_number.configure(text='Frame Number %d' % self.QRY_label_index)
-		
+
+	def go_to_frame(self, search):
+
+		if search == "REF_search":
+			REF_image = Image.open("/home/kylesm/Desktop/VRES/VRES_GUI/REF_frame_%d.jpg" % int(self.REF_entry.get()))
+			REF_photo = ImageTk.PhotoImage(REF_image)
+			self.REF_label.configure(image=REF_photo)
+			self.REF_label.image = REF_photo
+			self.REF_label_index = int(self.REF_entry.get())
+			self.REF_number.configure(text='Frame Number %d' % self.REF_label_index)
+
+		elif search == "QRY_search":
+			QRY_image = Image.open("/home/kylesm/Desktop/VRES/VRES_GUI/QRY_frame_%d.jpg" % int(self.QRY_entry.get()))
+			QRY_photo = ImageTk.PhotoImage(QRY_image)
+			self.QRY_label.configure(image=QRY_photo)
+			self.QRY_label.image = QRY_photo
+			self.QRY_label_index = int(self.QRY_entry.get())
+			self.QRY_number.configure(text='Frame Number %d' % self.QRY_label_index)
+
+
+
 
 if __name__ == "__main__":
 
 	#videos to be loaded
 	REF_vid_url = '/home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.mov'
-	QRY_vid_url = '/home/kylesm/Desktop/VRES/VRES_GUI/sync_iphone.mov'
+	QRY_vid_url = '/home/kylesm/Desktop/VRES/VRES_GUI/sync_sony.MTS'
 
 	root = Tk()
 	
