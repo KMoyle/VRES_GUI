24ffebc23830d3b4d480ad3a57daa76f9440c9d2
KMoyle <kylesmoyle@gmail.com>
2018-01-23 22:22:30 +1000

    Add files via upload

--- AnalysisScript.py
+++ AnalysisScript.py
@@ -48,29 +48,15 @@ def GetTemplateRegion(template_size, image_width, image_height):
 	template_region[2] = int(template_size * float(image_width))
 	template_region[3] = int(template_size * float(image_height))
 
-	if image_index == 1:
-		template_region[0] = int(float(image_width)/2.0 - float(template_region[2])/2.0)
-		template_region[1] = 0
-	else:
-		template_region[0] = int(float(image_width)/2.0 - float(template_region[2])/2.0) + pixel_shift[0]
-		template_region[1] = int(float(image_height)/2.0 - float(template_region[3])/2.0) + pixel_shift[1]
+
+	template_region[0] = int(float(image_width)/2.0 - float(template_region[2])/2.0) + pixel_shift[0]
+	template_region[1] = int(float(image_height)/2.0 - float(template_region[3])/2.0) + pixel_shift[1]
 
 	template_region[0] = min(max(template_region[0], 0), image_width - template_region[2])
 	template_region[1] = min(max(template_region[1], 0), image_height - template_region[3])
 
 	return template_region
 
-def GetPredictionRegion(ave_pixel_shifts, template_region, image_width, image_height, region_padding = 0):
-	prediction_region = [0, 0, image_width, image_height]
-	prediction_region[0] = template_region[0] - ave_pixel_shift[0] - region_padding
-	prediction_region[1] = template_region[1] - ave_pixel_shift[1] - region_padding
-	prediction_region[2] = template_region[2] + 2*region_padding
-	prediction_region[3] = template_region[3] + 2*region_padding
-
-	prediction_region[0] = min(max(prediction_region[0], 0), image_width - prediction_region[2])
-	prediction_region[1] = min(max(prediction_region[1], 0), image_height - prediction_region[3])
-
-	return prediction_region
 
 def VisualOdometryVisualizations(current_image, previous_image, template_region, prediction_region = None):
 	# DRAW RECTANGLES ON IMAGES
@@ -100,7 +86,15 @@ if __name__ == "__main__":
 	QRY_SURFACE_IMAGE_SET = "REF_surface_images"
 	QRY_FORWARD_IMAGE_SET = "REF_forward_images"
 
+	# LOAD IN PARAMETER FILE
 	PARAMETER_FILE = '/home/kylesm/Desktop/VRES/map_generation/ParameterFile.yaml'
+	parameter_file = IDME.ParameterFile(PARAMETER_FILE)
+
+	# LOAD IN PARMETERS
+	TEMPLATE_SIZE = GetParameterValue(parameter_file["Template_Matching_Parameters"], "Template_Size")
+	GEOMETRIC_PIXEL_SCALE = GetParameterValue(parameter_file["Visual_Odometry_Parameters"], "Geometric_Pixel_Scale")
+	X_CAM = GetParameterValue(parameter_file["Visual_Odometry_Parameters"], "X_Cam")
+	FILTER_SIZE = GetParameterValue(parameter_file["Visual_Odometry_Parameters"], "Filter_Size")
 
 	# INITIALIZE DATASET FILE, GET IMAGE SET AND IMAGE FILENAMES
 	REF_dataset = IDME.DatasetFile(REF_DATASET_PATH + REF_DATASET_FILE_NAME)
@@ -133,12 +127,43 @@ if __name__ == "__main__":
 
 	step = 30
 	# PERFORM VISUAL ODOMETRY
+	SAVE_ANALYSIS = True	
+	START_FRAME = 0
+	END_FRAME = 1198
 	REF_image_index = 0
 	QRY_image_index = 0
 
 	REF_forward_START_FRAME = 0
 	REF_array = 0
 	QRY_array = 0
+
+	# CREATE RESULTS SAVE DIRECTORY
+	REF_results_folder = REF_DATASET_PATH + "/Analyses"
+	if SAVE_ANALYSIS:
+		REF_results_folder =  REF_DATASET_PATH + "/Analyses/Mapping_Analysis" + str(REF_dataset.NumberOfAnalyses("Mapping_Analysis"))
+
+	if not os.path.exists(REF_results_folder):
+		os.makedirs(REF_results_folder)
+
+	# CREATE RESULTS SAVE DIRECTORY
+	QRY_results_folder = QRY_DATASET_PATH + "/Analyses"
+	if SAVE_ANALYSIS:
+		QRY_results_folder =  QRY_DATASET_PATH + "/Analyses/Mapping_Analysis" + str(QRY_dataset.NumberOfAnalyses("Mapping_Analysis"))
+
+	if not os.path.exists(QRY_results_folder):
+		os.makedirs(QRY_results_folder)
+
+	# INITIALIZE ANALYSIS FILE, COPY PARAMETERS AND METADATA AND ADD VISUAL ODOMETRY FIELD
+	REF_analysis_file = IDME.GenericAnalysisFile("Mapping_Analysis", REF_dataset.GetDatasetFileInformation(), REF_results_folder + '/Mapping_Analysis.yaml')
+	REF_analysis_file.CopyParameterSetFromFile(parameter_file)
+	REF_analysis_file.CopyImageSetMetadataFrom(REF_surface_image_set, [])
+	REF_analysis_file.AddAnalysisField("position_in_reference_map", IDME.kValidTypes.CV_POINT3d)
+
+	# INITIALIZE ANALYSIS FILE, COPY PARAMETERS AND METADATA AND ADD VISUAL ODOMETRY FIELD
+	QRY_analysis_file = IDME.GenericAnalysisFile("Mapping_Analysis", QRY_dataset.GetDatasetFileInformation(), QRY_results_folder + '/Mapping_Analysis.yaml')
+	QRY_analysis_file.CopyParameterSetFromFile(parameter_file)
+	QRY_analysis_file.CopyImageSetMetadataFrom(QRY_surface_image_set, [])
+	QRY_analysis_file.AddAnalysisField("position_in_reference_map", IDME.kValidTypes.CV_POINT3d)
 
 	#REF_image = cv2.imread(REF_forward_image_set.Folder() + "/" + REF_forward_image_filenames[REF_image_index])
 	#cv2.imshow( "Display window", REF_image )
@@ -228,24 +253,42 @@ if __name__ == "__main__":
 		# if k == 27:         # wait for ESC key to exit
 		# 	cv2.destroyAllWindows()
 
+	# INIT VARIABLES
+	pixel_shift = [0, 0]
+	pixel_shift_history = []
+	for ii in range(0, FILTER_SIZE):
+		pixel_shift_history.append([None, None])
+	position_estimates = np.zeros((END_FRAME-START_FRAME+1, 3), float)
+
+	# INIT DISPLAY WINDOWS
+	cv2.namedWindow("QRY Frame", cv2.WINDOW_NORMAL)
+	cv2.namedWindow("REF Frame", cv2.WINDOW_NORMAL)
+
+	array_index = 0
 	# PERFORM SURFACE IMAGE POSE ESTIMATION
 	for image_index in range(0, (len(bestmatch_array))):
-
+		array_index = array_index + 1
+		idx = bestmatch_array[image_index]
+		
 		# LOAD IN SPECIFIC SURFACE IMAGES
-		QRY_surface_image = cv2.imread(QRY_surface_image_set.Folder() + "/" + QRY_surface_image_filenames[idx*step])
-		QRY_surface_image_resized = cv2.resize(QRY_surface_image, (600,480))
-
-		REF_surface_image = cv2.imread(REF_surface_image_set.Folder() + "/" + REF_surface_image_filenames[best_match_index*step])
-		REF_surface_image_resized = cv2.resize(REF_surface_image, (600, 480))
+		QRY_surface_image = cv2.imread(QRY_surface_image_set.Folder() + "/" + QRY_surface_image_filenames[image_index*step], cv2.IMREAD_GRAYSCALE)
+		REF_surface_image = cv2.imread(REF_surface_image_set.Folder() + "/" + REF_surface_image_filenames[idx*step*2], cv2.IMREAD_GRAYSCALE)
 
 		# GET TEMPLATE REGION AND IMAGE
-		im_height, im_width = QRY_surface_image_resized.shape[:2]
+		im_height, im_width = QRY_surface_image.shape[:2]
 		template_region = GetTemplateRegion(TEMPLATE_SIZE, im_width, im_height)
-		template = previous_image[template_region[1]:template_region[1]+template_region[3], template_region[0]:template_region[0]+template_region[2]]
-
+		template = QRY_surface_image[template_region[1]:template_region[1]+template_region[3], template_region[0]:template_region[0]+template_region[2]]
+		
 		# GET PREDICTION REGION AND IMAGE
 		prediction_region = [0, 0, im_height, im_width]
-		prediction_region_image = REF_surface_image_resized
+		prediction_region_image = REF_surface_image
+		
+		# PERFORM TEMPLATE MATCHING
+		match_result = cv2.matchTemplate(prediction_region_image, template, cv2.TM_CCOEFF_NORMED)
+		min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(match_result)
+
+		print image_index*step
+		print idx*step*2
 
 		# GET MATCHED REGION RECTANGLE LOCATION
 		rect_loc = [0,0]
@@ -253,7 +296,37 @@ if __name__ == "__main__":
 		rect_loc[1] = max_loc[1]
 
 		# VISUALIZATIONS
-		VisualOdometryVisualizations(current_image, previous_image, template_region)
+		VisualOdometryVisualizations(REF_surface_image, QRY_surface_image, template_region)
+
+		# END OF LOOP - UPDATE PREVIOUS IMAGE, UPDATE PIXEL SHIFTS, GET POSITION ESTIMATION, PERFORM POSITION SHIFT MOVING AVERAGING FILTER
+		first_iteration = False
+		pixel_shift[0] = template_region[0] - rect_loc[0]
+		pixel_shift[1] = template_region[1] - rect_loc[1]
+
+		pixel_shift_history = pixel_shift_history[-1:] + pixel_shift_history[:-1]
+		pixel_shift_history[0][0] = pixel_shift[0]
+		pixel_shift_history[0][1] = pixel_shift[1]
+
+		# POSITION ESTIMATES USING PROCESS DEFINED IN NOURANI VISUAL ODOMETRY PAPERS
+		delta_x = -pixel_shift[1] * GEOMETRIC_PIXEL_SCALE
+		delta_theta = -float(pixel_shift[0])/float(X_CAM) * GEOMETRIC_PIXEL_SCALE
+
+		position_estimates[array_index, 2] = 0 + delta_theta
+		position_estimates[array_index, 0] = 0 + delta_x*np.cos(position_estimates[array_index-1, 2]) 
+		position_estimates[array_index, 1] = 0 + delta_x*np.sin(position_estimates[array_index-1, 2])
+
+		
+		print "x= %.3f" % position_estimates[array_index, 0]
+		print "y= %.3f" % position_estimates[array_index, 1]
+		print "theta= %.3f" % position_estimates[array_index, 2]
+
+		raw_input("press enter")
+
+		# # SET VO POSITION IN ANALYSIS FILE
+		# error = analysis_file.SetAnalysisValue(filename, "VO_Pos", (position_estimates[array_index, 0], position_estimates[array_index, 1], position_estimates[array_index, 2]))
+		# if error != IDME.IDME_OKAY:
+		# 	print "WARNING! Was unable to set the VO Position for image %s to (%d, %d, %d). Error Code %d"%(filename, position_estimates[array_index, 0], position_estimates[array_index, 1], position_estimates[array_index, 2], error)
+
 
 	
 
